8e2452c9bfea557ad09edbe7d6ea768c837d9b4c
Mudita Bysani <muditabysani@muditas-mbp.home>
2019-09-05 22:14:42 -0700

    Finished the Trie implementations

--- Problem1.py
+++ Problem1.py
@@ -40,3 +40,58 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+class TrieNode:
+	def __init__(self, val):
+		self.val = val
+		self.children = [None]*26
+		self.isword = False
+
+class Trie:
+
+	def __init__(self):
+		"""
+		Initialize your data structure here.
+		"""
+		self.root = TrieNode(" ")
+
+	def insert(self, word: str) -> None:
+		"""
+		Inserts a word into the trie.
+		"""
+		temp = self.root
+		for i in word:
+			node = TrieNode(i)
+			if temp.children[ord(i) - ord('a')] == None:
+				temp.children[ord(i) - ord('a')] = node
+			temp = temp.children[ord(i) - ord('a')]
+		temp.isword = True
+
+	def search(self, word: str) -> bool:
+		"""
+		Returns if the word is in the trie.
+		"""
+		temp = self.root
+		for i in word:
+			if temp.children[ord(i) - ord('a')] == None:
+				return False
+			temp = temp.children[ord(i) - ord('a')]
+		return temp.isword
+
+	def startsWith(self, prefix: str) -> bool:
+		"""
+		Returns if there is any word in the trie that starts with the given prefix.
+		"""
+		temp = self.root
+		for i in prefix:
+			if temp.children[ord(i) - ord('a')] == None:
+				return False
+			temp = temp.children[ord(i) - ord('a')]
+		return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)

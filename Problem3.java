c922f6e34cb82e6a68ad5d72d4c4c5de2ebab797
Prateek Dasgupta <dasguptp@oregonstate.edu>
2020-01-05 07:03:58 -0800

    Adding Problem 3

---
+++ Problem3.java
@@ -0,0 +1,88 @@
+/**
+LeetCode Submitted : YES
+Time Complexity : O(Max Depth of the Trie Structure)
+Space Complexity : O(Num of words in Dictionary)
+
+The idea is to use Trie data structure to store words in dictionary. Now iterate each words in the sentence and upon finding a word in dictionary we truncate the search and returns the first word in dictionary.
+**/
+
+class Solution {
+    
+    class Trie {
+        class Node{
+            Node[] children = new Node[26]; //char Array
+            String value;
+            public Node(){
+            
+            }
+        }
+    
+        Node root;
+    
+        /** Initialize your data structure here. */
+        public Trie() {
+            root = new Node();
+        }
+    
+        /** Inserts a word into the trie. */
+        public void insert(String word) {
+            Node temp = root;
+            for(int i = 0; i < word.length(); i++){
+                char c = word.charAt(i);
+                //System.out.println(c-'a');
+                if(temp.children[c - 'a'] == null)
+                    temp.children[c - 'a'] =  new Node();
+            
+                //temp.children[c - 'a'].value =  1;
+                temp = temp.children[c - 'a'];
+            }
+            temp.value = word;
+        }
+    
+        /** Returns if the word is in the trie. */
+        public String search(String word) {
+            Node temp = root;
+            for(int i = 0; i < word.length(); i++){
+                char c = word.charAt(i);
+                if(temp.children[c - 'a'] != null)
+                {
+                    if(temp.children[c - 'a'].value != null)
+                        return temp.children[c - 'a'].value;
+                    temp = temp.children[c - 'a'];   
+                }else
+                    return word;
+            }
+            return word;
+        }
+        
+        public String replaceWords(String sentence){
+            Node node = root;
+            StringBuffer output = new StringBuffer();
+            
+            String[] words =  sentence.split(" ");
+            int l =  words.length - 1;
+            //System.out.println(l);
+            for(String word : words){
+                String result = search(word);
+                output.append(result);
+                if(l > 0)
+                    {
+                        output.append(" ");
+                        l--;
+                    }
+                }
+            
+            return output.toString();
+        }
+        
+    }
+ 
+    public String replaceWords(List<String> dict, String sentence) {
+        Trie obj = new Trie();
+        for(String word : dict)
+            obj.insert(word);
+        
+        //helper function to find maximum word 
+        return obj.replaceWords(sentence);
+    }
+}

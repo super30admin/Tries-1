54ab831bdc402f17e12b4a87b83de66058459f72
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2020-01-05 22:35:47 -0800

    Create Longest_word_in_list.py

---
+++ Longest_word_in_list.py
@@ -0,0 +1,38 @@
+# Runs on Leetcode
+    # Runtime - O()
+    # Memory - O()
+
+
+class Solution:
+    class TrieNode:
+        def __init__(self):
+            self.children = [None]*26
+            self.string = None
+            
+    def __init__(self):
+        self.root = self.TrieNode()
+        
+    def insert(self,word):
+        node = self.root
+        for i in word:
+            if not node.children[ord(i)-ord('a')]:
+                node.children[ord(i)-ord('a')] = self.TrieNode()
+            node = node.children[ord(i)-ord('a')]
+        node.string = word
+        
+    def longestWord(self, words: List[str]) -> str:
+        for i in words:
+            self.insert(i)
+        queue = []
+        queue.append(self.root)
+        
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.pop(0)
+                for j in range(25,-1,-1):
+                    if node.children[j] and node.children[j].string:
+                        queue.append(node.children[j])
+                    else:
+                        continue
+        return node.string

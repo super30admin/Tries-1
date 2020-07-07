3a1faf5cf332cf459c6b9109a361854a23108778
chirag <cr.tagadiya@gmail.com>
2020-07-07 16:09:35 -0400

    Longest word using DFS

--- LongestWord.java
+++ LongestWord.java
@@ -90,3 +90,77 @@ public String longestWord(String[] words) {
     }
 
 */
+
+/*
+
+
+
+
+/*
+
+Longest Word using DFS
+
+
+class Solution {
+    Trie root;
+    class Trie{
+        String word;
+        Trie [] children;
+        
+        public Trie(){
+            children = new Trie[26];
+        }
+        
+    }
+    
+    
+    private void add_word(String word){
+        Trie cur = root;
+        for( int i =0; i < word.length() ; i++){
+            char c = word.charAt(i);
+            
+            if(cur.children[c - 'a'] == null){
+                cur.children [c - 'a'] = new Trie();
+            }
+            cur = cur.children[c -  'a'];
+        }
+        
+        cur.word = word;
+    }
+    public String longestWord(String[] words) {
+        root = new Trie();
+        
+//       add words in Trie for processing
+        for(String word :  words){
+            add_word(word);
+        }
+        String res ="";
+        Stack<Trie> st = new Stack();
+        st.add(root);
+        Trie node = null;
+        while(!st.isEmpty()){
+            
+            node  =  st.pop();
+            
+            if(node.word != null){
+                if(node.word.length() > res.length()){
+                    res = node.word;
+                }
+                if(node.word.length() == res.length() && node.word.compareTo(res) < 0){
+                    res = node.word;
+                }
+            }
+            
+            Trie [] children = node.children;
+            for(int i=0; i < 26; i++){
+                if( children[i] != null && children[i].word != null){
+                    st.add(children[i]);
+                }
+            }
+        }
+        
+        return res;
+    }
+}
+
+*/

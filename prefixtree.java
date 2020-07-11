d8ff2933636bec432f54fabe660e1421ae48b7af
Gahan Jagadeesh <gahanjagadeesh@gmail.com>
2020-07-11 16:22:59 -0400

    Completed Tries

---
+++ prefixtree.java
@@ -0,0 +1,56 @@
+//Time complexity:O(l) where l is length of word
+//Space complexity:O(l) for insert and O(1) for rest
+
+class Trie {
+    class TrieNode{
+        boolean isEnd;
+        TrieNode[] children;
+        public TrieNode(){
+            children=new TrieNode[26];
+        }
+    }
+    TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        root=new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            if(curr.children[c-'a']==null){
+                curr.children[c-'a']=new TrieNode();
+            }
+            curr=curr.children[c-'a'];
+        }
+        curr.isEnd=true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            if(curr.children[c-'a']==null){
+                return false;
+            }
+            curr=curr.children[c-'a'];
+        }
+        return curr.isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode curr=root;
+        for(int i=0;i<prefix.length();i++){
+            char c=prefix.charAt(i);
+            if(curr.children[c-'a']==null){
+                return false;
+            }
+            curr=curr.children[c-'a'];
+        }
+        return true;
+    }
+}

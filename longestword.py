d514888f42aa40d2ebd8d4b10d9b6d588164f8d6
saikumarreddy_sandannagari <saikumarreddy0248@gmail.com>
2020-08-17 13:16:51 -0700

    Add files via upload

---
+++ longestword.py
@@ -0,0 +1,8 @@
+class Solution():
+    def longestWord(self, words):
+        wordset = set(words)
+        words.sort(key = lambda c: (-len(c), c))
+        for word in words:
+            if all(word[:k] in wordset for k in range(1, len(word))):
+                return word
+        return ""

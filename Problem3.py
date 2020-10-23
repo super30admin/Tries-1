9d230e4ed46f6ce12fcbc69875f9a703b68dc5ff
koshyanand <koshyanand@gmail.com>
2020-10-22 20:42:41 -0700

    Completed 3

--- Problem3.py
+++ Problem3.py
@@ -1,36 +1,49 @@
+# Time Complexity :  O(N) Sum of all characters in the sentence
+# Space Complexity: O(N)
+# Passed Leetcode 
+
+class TrieNode:
+    
+    def __init__(self):
+        
+        self.valid = False
+        
+        self.next_nodes = [None for i in range(26)]
+        
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         
-        trie = {}
+        trie = TrieNode()
         
+        dict_set = set()
         for word in dictionary:
             temp = trie
             for w in word:
-                
-                if w not in temp:
-                    temp[w] = {}
-                temp = temp[w]
-        
-        word_list = sentence.split()
+                if not temp.next_nodes[ord(w) - 97]:
+                    temp.next_nodes[ord(w) - 97] = TrieNode()
+                    
+                temp = temp.next_nodes[ord(w) - 97]
+            temp.valid = True
+            
+        word_list = sentence.split(" ")
         
         out = []
         for word in word_list:
             temp = trie
-            
-            if word[0] not in temp:
+            if not temp.next_nodes[ord(word[0]) - 97]:
                 out.append(word)
                 continue
-            for i, w in enumerate(word):
-                
-                if w not in temp:
-                    
-                    if len(temp.items()) == 0:
-                        out.append(word[:i])                        
-                    else:
-                        out.append(word)
-                        
+            
+            out_word = word
+            for i, w in enumerate(word):   
+                if temp.valid:
+                    out_word = word[:i]
                     break
-                temp = temp[w]
+                if not temp.next_nodes[ord(w) - 97]:
+                    break
+                
+                temp = temp.next_nodes[ord(w) - 97]
+            out.append(out_word)
         return " ".join(out)
                 
                 

f9306a3d6e344e6106414d14947d5f8446965e83
stuti <stuti@DESKTOP-1KQ9LMO>
2021-03-28 22:37:06 -0400

    done

---
+++ prob77.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestWord(String[] words) {
+        
+        Arrays.sort(words);
+        HashSet<String> builtWords = new HashSet();
+        String result ="";
+        for(String w : words){
+            if(w.length() == 1 || builtWords.contains(w.substring(0, w.length()-1))){
+                if(w.length ()> result.length())
+                    result = w;
+                builtWords.add(w);
+            }
+        }
+        return result;
+        
+    }
+}

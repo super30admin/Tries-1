a3c9c58dac2f6cb6b936916e6d63ee0785236034
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-07-02 20:33:17 +0530

    Completed Problem 1,2,3

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,62 @@
-# Time Complexity : O()
-# Space Complexity : O()
+# Time Complexity : insert,search,prefix- O(len(s))
+# Space Complexity : O(number of string * len(strings))
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
+
+#make is end if the a word exists
+class TrieNode:
+    def __init__(self):
+        self.isEnd = False
+        self.Childs = [None]*26
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        
+        for i in range(len(word)):
+            if node.Childs[ord(word[i]) - 97] == None:
+                node.Childs[ord(word[i]) - 97] = TrieNode()
+            node = node.Childs[ord(word[i]) - 97]
+        node.isEnd = True
+        return;
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for i in range(len(word)):
+            if node.Childs[ord(word[i]) - 97] == None:
+                return False
+            node = node.Childs[ord(word[i]) - 97]
+        return node.isEnd
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for i in range(len(prefix)):
+            if node.Childs[ord(prefix[i]) - 97] == None:
+                return False
+            node = node.Childs[ord(prefix[i]) - 97]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,57 @@
-# Time Complexity : O()
-# Space Complexity : O()
+# Time Complexity : O(n*k)
+# Space Complexity : O(n*k)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
+
+#construct trie and check if substring is a word in dictionary
+class TrieNode:
+    def __init__(self):
+        self.isEnd = False
+        self.Childs = [None]*26
+class Solution:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        
+        for i in range(len(word)):
+            if node.Childs[ord(word[i]) - 97] == None:
+                node.Childs[ord(word[i]) - 97] = TrieNode()
+            node = node.Childs[ord(word[i]) - 97]
+        node.isEnd = True
+        return;
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        for i in range(len(dictionary)):
+            self.insert(dictionary[i])
+        temp = sentence.split(" ")
+        words = []
+        for i in temp:
+            if i != " ":
+                words.append(i)
+        
+        for i in range(len(words)):
+            word = words[i]
+            node = self.root
+            exists = True
+            newWord = ""
+            for j in range(len(word)):
+                char = word[j]
+                if node.isEnd:
+                    exists = True
+                    break
+                if node.Childs[ord(char)-97] == None:
+                    exists = False
+                    break
+                newWord += char
+                node = node.Childs[ord(char)-97]
+            if node.isEnd or exists:
+                words[i] = newWord
+        return " ".join(words)

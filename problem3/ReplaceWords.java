aae28bd2258155722df023ab18a9d7c8c803fb9d
Srirag <78050630+sriragcs@users.noreply.github.com>
2021-07-29 20:40:39 -0700

    Changed code for Replace Words

--- problem3/ReplaceWords.java
+++ problem3/ReplaceWords.java
@@ -1,5 +1,5 @@
-//Time Complexity : O(n), n -> Length of sentence
-// Space Complexity : O(n)
+//Time Complexity : O(nk + l), n -> Number of words in dictionary, k -> Length of each word, l -> Length of sentence
+// Space Complexity : O(nk)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 package problem3;
@@ -9,6 +9,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ReplaceWords {
+	TrieNode root;
+
+	public void insert(String word) {
+		TrieNode node = root;
+		for (char ch : word.toCharArray()) {
+			if (node.nodes[ch - 'a'] == null) {
+				node.nodes[ch - 'a'] = new TrieNode();
+			}
+			node = node.nodes[ch - 'a'];
+		}
+		node.isWord = true;
+	}
+
 	/*************************************************************
 	 * Store root words in a trie, then traverse sentence to replace words
 	 *************************************************************/
@@ -21,50 +34,35 @@ public class ReplaceWords {
 			return sentence;
 		}
 
+		root = new TrieNode();
 		// Add nodes to trie
-		TrieNode root = new TrieNode();
 		for (String word : dictionary) {
-			TrieNode node = root;
-			for (char ch : word.toCharArray()) {
-				if (!node.containsKey(ch)) {
-					node.put(ch, new TrieNode());
-				}
-				node = node.get(ch);
-			}
-			node.setWord();
+			insert(word);
 		}
 
 		StringBuilder sb = new StringBuilder();
 
-		for (String word : sentence.split(" ")) {
+		for (String word : sentence.split("\\s+")) {
 			TrieNode node = root;
-			boolean flag = true;
 			StringBuilder curWord = new StringBuilder();
-			int i = 0;
-			for (i = 0; i < word.length(); i++) {
+
+			for (int i = 0; i < word.length(); i++) {
 				char ch = word.charAt(i);
+				if (node.nodes[ch - 'a'] == null || node.isWord) {
+					break;
+				}
 				curWord.append(ch);
-				if (node.get(ch) == null) {
-					flag = false;
-					break;
-				}
-				if (node.get(ch).isWord()) {
-					break;
-				}
-				node = node.get(ch);
+				node = node.nodes[ch - 'a'];
 			}
-			if (i != word.length()) {
-				if (flag) {
-					sb.append(curWord);
-				} else {
-					sb.append(word);
-				}
+
+			if (node.isWord) {
+				sb.append(curWord);
 			} else {
 				sb.append(word);
 			}
 			sb.append(" ");
 		}
-		return sb.toString();
+		return sb.toString().trim();
 	}
 
 	public static void main(String[] args) {
--- problem3/TrieNode.java
+++ problem3/TrieNode.java
@@ -1,31 +1,11 @@
 package problem3;
 
 public class TrieNode {
-	private TrieNode[] nodes;
-	private boolean isWord;
+	TrieNode[] nodes;
+	boolean isWord;
 
 	public TrieNode() {
 		nodes = new TrieNode[26];
 		isWord = false;
-	}
-
-	public boolean containsKey(char ch) {
-		return nodes[ch - 'a'] != null;
-	}
-
-	public TrieNode get(char ch) {
-		return nodes[ch - 'a'];
-	}
-
-	public void put(char ch, TrieNode node) {
-		nodes[ch - 'a'] = node;
-	}
-
-	public void setWord() {
-		isWord = true;
-	}
-
-	public boolean isWord() {
-		return isWord;
 	}
 }

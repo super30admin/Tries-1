05d27ce2291777df794010101e2e75075938081b
Adhish Shrivastava <adhish70@gmail.com>
2021-11-19 11:05:53 -0500

    Problem76 done

--- Problem-76 Implement Trie (Prefix Tree).py
+++ Problem-76 Implement Trie (Prefix Tree).py
@@ -6,23 +6,43 @@
 # Time Complexiety: 
 # Space Complexiety: 
 
-class Trie:
-
+class trieNode:
     def __init__(self):
-        pass
+        self.isEnd = False
+        self.children = dict()
+
+class Trie:
+    def __init__(self):
+        self.root = trieNode()
 
     def insert(self, word: str) -> None:
-        pass
+        curr = self.root
+        for i in word:
+            if curr.children.get(i) == None:
+                curr.children[i] = trieNode()
+            curr = curr.children[i]
+        curr.isEnd = True
 
     def search(self, word: str) -> bool:
-        pass
+        curr = self.root
+        for i in word:
+            if curr.children.get(i) == None:
+                return False
+            curr = curr.children[i]
+        return curr.isEnd == True
 
     def startsWith(self, prefix: str) -> bool:
-        pass
+        curr = self.root
+        for i in prefix:
+            if curr.children.get(i) == None:
+                return False
+            curr = curr.children[i]
+        return True
 
 
 # Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
+obj = Trie()
+obj.insert('adds')
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+print(obj.search('ad'))

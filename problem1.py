39bd4eed7e13f4e6b6057aff1574a772392c30fe
yashgovilkar <yashgovilkar14@gmail.com>
2021-12-07 20:56:44 +0530

    Completed Tries-1

---
+++ problem1.py
@@ -0,0 +1,38 @@
+#Did this code successfully run on Leetcode : yes
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+#Time complexity: O(n)
+#Space : O(n)
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isEnd = True
+        
+#Time Complexity: O(n)
+#Space: O(1)
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.isEnd
+        
+#Time Complexity: O(len(prefix))
+#Space : O(1)
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
---
+++ problem2.py
@@ -0,0 +1,37 @@
+#Time complexity: O(n*k)
+#Space complexity: O(n*k)
+#Did this code successfully run on Leetcode : yes
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.word=None
+    
+class Solution:
+   #Initializing trie node 
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self,word):
+        cur = self.root
+        for i in word:
+            if i not in cur.children:
+                cur.children[i]=TrieNode()
+            cur = cur.children[i]
+        cur.word = word
+        
+    def longestWord(self, words: List[str]) -> str:
+        #Create Trie
+        for i in words:
+            self.insert(i)
+        #Apply Bfs
+        bfs=deque()
+        bfs.append(self.root)
+        ans=""
+        while bfs:
+            cur = bfs.popleft()
+            for child in cur.children.values():
+                if child.word != None:
+                    if len(child.word) > len(ans) or len(child.word) == len(ans) and child.word < ans:
+                        ans = child.word
+                    bfs.append(child)
+        return ans

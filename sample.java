62a2e24201a5e4997b6d8eaf4601450572bb6e28
Ikshaku Goswami <ig752994@gmail.com>
2022-01-06 21:38:53 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -137,3 +137,70 @@ class Solution {
     }
 } 
 
+// replace words from a dictionary
+//tc length of sentence
+// sc length of sentence
+
+class Solution {
+    
+    class TrieNode {
+        boolean isEnd;
+        TrieNode [] children;
+
+        public TrieNode(){
+            children = new TrieNode[26];
+        }
+    }
+
+    TrieNode root;
+
+    private void insert(String word){
+        TrieNode curr = root;
+
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+
+            if(curr.children[ch-'a'] == null ){
+                curr.children[ch-'a'] = new TrieNode();
+            }
+            curr= curr.children[ch-'a'];
+        }
+        curr.isEnd= true;
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        
+        root = new TrieNode();
+
+        for(int i=0;i<dictionary.size();i++){
+            insert(dictionary.get(i));
+        }
+
+        String [] sentenceArray = sentence.split(" ");
+        StringBuilder replacement = new StringBuilder();
+        
+        for(String word: sentenceArray){
+
+            StringBuilder temp = new StringBuilder();
+            TrieNode curr = root;
+            
+            for(int i=0;i<word.length();i++){
+
+                char ch = word.charAt(i);
+                if(curr.children[ch-'a'] == null || curr.isEnd == true){
+                    break;
+                }
+                temp.append(ch);
+                curr= curr.children[ch-'a'];
+            }
+            if(curr.isEnd==true){
+                replacement.append(temp + " ");
+            }else{
+                replacement.append(word + " ");
+            }
+
+        }
+        return replacement.toString().trim();
+    }
+}
+

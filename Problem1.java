5b78445378279463c77d889eb18465d43bd62c8a
Rujal <rujal8452@gmail.com>
2022-05-12 00:44:57 -0400

    Problem1

---
+++ Problem1.java
@@ -0,0 +1,68 @@
+public class Problem1 {
+    class Trie {
+
+        class TrieNode {
+            TrieNode[] children;
+            boolean isEnd;
+
+            TrieNode() {
+                children = new TrieNode[26];
+            }
+        }
+
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        // TC : O (n)
+        // SC : O (n) where n = length of word
+        public void insert(String word) {
+            TrieNode curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (curr.children[c - 'a'] == null) {
+                    curr.children[c - 'a'] = new TrieNode();
+                }
+                curr = curr.children[c - 'a'];
+            }
+            curr.isEnd = true;
+        }
+
+        // TC : O (n) where n = word length
+        // SC : O (1)
+        public boolean search(String word) {
+            TrieNode curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (curr.children[c - 'a'] == null) {
+                    return false;
+                }
+                curr = curr.children[c - 'a'];
+            }
+            return curr.isEnd;
+        }
+
+        // TC : O (n) where n = prefix length
+        // SC : O (1)
+        public boolean startsWith(String prefix) {
+            TrieNode curr = root;
+            for (int i = 0; i < prefix.length(); i++) {
+                char c = prefix.charAt(i);
+                if (curr.children[c - 'a'] == null) {
+                    return false;
+                }
+                curr = curr.children[c - 'a'];
+            }
+            return true;
+        }
+    }
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+}

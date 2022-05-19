cf9fc579ed00e39ab4ac7ca626a38d207c3de0e3
Vaishnavi Kannan <v.kannan@salesforce.com>
2022-05-18 20:34:00 -0700

    longest word - BFS

--- longestWordInDictionary.java
+++ longestWordInDictionary.java
@@ -78,3 +78,70 @@ class Solution {
         return answer;
     }
 }
+
+
+// BFS Solution
+class TrieNode {
+    TrieNode children[] = null;
+    String word;
+    
+    public TrieNode() {
+        children = new TrieNode[26];
+        word = null;
+    }
+}
+
+class Trie {
+    TrieNode root = null;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void buildTrie(String words[]) {
+        for (String word : words) {
+            insert(word);
+        }
+    }
+    
+    public void insert(String word) {
+        TrieNode cur = root;
+        
+        for (int i = 0; i < word.length(); ++i) {
+            int index = word.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                cur.children[index] = new TrieNode();
+            }
+            cur = cur.children[index];
+        }
+        cur.word = word;
+    }
+    
+    public TrieNode getRoot() {
+        return root;
+    }
+}
+
+class Solution {
+    Trie trie = null;
+    
+    public String longestWord(String[] words) {
+        trie = new Trie();
+        trie.buildTrie(words);
+        TrieNode cur = trie.getRoot();
+        Queue<TrieNode> queue = new LinkedList<>();
+        queue.add(cur);
+        
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            for (int i = 25; i >= 0; --i) {
+                if (cur.children[i] != null && cur.children[i].word != null) {
+                    queue.offer(cur.children[i]);
+                }
+            }
+        }
+        
+        if (cur.word == null) return "";
+        return cur.word;
+    }
+}

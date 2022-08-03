f18294bc133115b91b6a8cb78a65c9b97b54d92d
siddbanerjee2012 <sidd.banerjee2012@gmail.com>
2022-08-02 22:00:39 -0500

    Completed Sprint Tries-1

---
+++ implement_trie.java
@@ -0,0 +1,56 @@
+// Time Complexity :O(mn)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+class Trie {
+    class TrieNode{
+        TrieNode[] children;
+        boolean isEnd;
+        public TrieNode(){
+            children=new TrieNode[26];
+        }
+    }
+    TrieNode root;
+    public Trie() {
+        root=new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            if(curr.children[c - 'a']==null){
+                curr.children[c - 'a']=new TrieNode();
+            }
+            curr=curr.children[c - 'a'];
+        }
+        curr.isEnd=true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            if(curr.children[c - 'a']==null){
+                return false;
+            }
+            curr=curr.children[c - 'a'];
+        }
+        return curr.isEnd;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode curr=root;
+        for(int i=0;i<prefix.length();i++){
+            char c=prefix.charAt(i);
+            if(curr.children[c - 'a']==null){
+                return false;
+            }
+            curr=curr.children[c - 'a'];
+        }
+        return true;
+    }
+}

a5c5252509ec514f6ea33836643d4d24a48e484d
Thirunaa <thirunaavukkarasu.thiru@gmail.com>
2022-09-13 13:56:22 -0400

    Completed all the problems

---
+++ Longest word in Dictionary.py
@@ -0,0 +1,50 @@
+class TrieNode:
+    def __init__(self):
+        self.isEnd = False
+        self.children = [None] * 26
+             
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def getRoot(self):
+        return self.root
+    
+    def insert(self,word):
+        curr = self.root
+        for char in word:
+            index = ord(char)-ord('a')
+            if curr.children[index]==None:
+                curr.children[index]=TrieNode()
+                
+            curr = curr.children[index]
+            
+        curr.isEnd = True
+        
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        root = trie.getRoot()
+        self.result = []
+        self.backtrack(root,[])
+        return "".join(self.result)
+        
+    def backtrack(self,root,longword):
+        #base
+        if len(self.result)<len(longword):
+            self.result = longword[:]
+        
+        #logic
+        for index in range(26):
+            if root.children[index] and root.children[index].isEnd==True:
+                #action
+                longword.append(chr(index+ord('a')))
+                
+                #recurse
+                self.backtrack(root.children[index],longword)
+                
+                #backtrack
+                longword.pop()
+            

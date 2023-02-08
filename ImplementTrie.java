a6c5b2bc53abf7b909ffa63e7454fce206f2dfbc
Jackson Raj A <jacksonraj1095@gmail.com>
2023-02-08 22:42:30 +0530

    Completed Implementation of Tries-1

---
+++ ImplementTrie.java
@@ -0,0 +1,61 @@
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+public class ImplementTrie {
+
+    static class Trie {
+
+        class TrieNode {
+            boolean isEnd;
+            TrieNode[] children;
+
+            public TrieNode() {
+                children = new TrieNode[26];
+            }
+        }
+
+        TrieNode root;
+
+        public Trie() {
+            this.root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (curr.children[c - 'a'] == null) {
+                    curr.children[c - 'a'] = new TrieNode();
+                }
+                curr = curr.children[c - 'a'];
+            }
+            curr.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            TrieNode curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (curr.children[c - 'a'] == null) {
+                    return false;
+                }
+                curr = curr.children[c - 'a'];
+            }
+            return curr.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode curr = root;
+            for (int i = 0; i < prefix.length(); i++) {
+                char c = prefix.charAt(i);
+                if (curr.children[c - 'a'] == null) {
+                    return false;
+                }
+                curr = curr.children[c - 'a'];
+            }
+            return true;
+        }
+    }
+}

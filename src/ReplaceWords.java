eb7e2f5dc847a90d601400ebc31037f918c71490
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-08-31 15:09:31 -0700

    Update ReplaceWords.java

--- src/ReplaceWords.java
+++ src/ReplaceWords.java
@@ -31,3 +31,44 @@ public class ReplaceWords {
         return sb.toString();
     }
 }
+
+class TrieNode {
+    TrieNode[] children;
+    boolean end;
+    public TrieNode() {
+        children = new TrieNode[26];
+        end = false;
+    }
+}
+
+class Trie {
+    public TrieNode root;
+    public Trie() {
+        root = new TrieNode();  
+    }
+    public void insert(String word) {
+        TrieNode node = root;
+        for(int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.end = true;
+    }
+    public String getPrefixWord(String word) {
+        TrieNode node = root;
+        for(int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(node.children[c - 'a'] == null) {
+                return null;
+            }
+            node = node.children[c - 'a'];
+            if(node.end == true) {
+                return word.substring(0,i+1);
+            }
+        }
+        return null;
+    }
+}

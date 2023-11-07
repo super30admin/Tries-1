1711fb36f626da56c032ed79eeae343bc755fa3c
Mohini Mayekar <mmayekar97@gmail.com>
2023-11-07 17:20:34 -0500

    Tries-1 Done

---
+++ TrieImplementation/TrieImplementation.java
@@ -0,0 +1,70 @@
+/* Time Complexity : O(L) 
+ * 	L - length of the input string - word 
+ *  search, insert, startsWith - O(L) each*/
+/* Space Complexity : O(nl)
+ *	n - number of words in the dictionary
+ *	l - avg. len. of the words
+ *	search, insert, startsWith - O(1) */
+// Did this code successfully run on Leetcode : Yes 
+// Any problem you faced while coding this :
+
+class Trie {
+
+    class TrieNode{
+        boolean isEnd;
+        TrieNode [] children;
+        public TrieNode(){
+            this.children = new TrieNode[26];
+        }
+    }
+    
+    TrieNode root;
+    public Trie(){
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(curr.children[c - 'a'] == null){
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isEnd = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(curr.children[c - 'a'] == null){
+                return false;
+            }
+            curr = curr.children[c - 'a'];
+        }
+        return curr.isEnd;
+    }
+
+    
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for(int i = 0; i < prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(curr.children[c - 'a'] == null){
+                return false;
+            }
+            curr = curr.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
